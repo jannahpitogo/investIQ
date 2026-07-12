6b020f8419939ff2d55c9689472659f6fe36e94c
jannahpitogo <pitogojannah@outlook.com>
2026-07-13 01:19:02 +0200

    add: mock console.log implementation in beforeEach for tests (for clean tests output)

--- backend/src/test/backend.test.js
+++ backend/src/test/backend.test.js
@@ -16,3 +16,7 @@ import {
   analyzeEnvironmentalImpact,
   analyzeSocialImpact,
 } from '../services/portfolioService.js'
+
+beforeEach(() => {
+  vi.spyOn(console, 'log').mockImplementation(() => {})
+})
