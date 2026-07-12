ab9ec63ee506581e9eff56bd6e41474869c1306d
jannahpitogo <pitogojannah@outlook.com>
2026-07-13 01:17:23 +0200

    add: imported the necessary functions from the backend and data for BOYCOTT LIST

---
+++ backend/src/test/backend.test.js
@@ -0,0 +1,18 @@
+import { test, expect, vi, beforeEach } from 'vitest'
+import { BOYCOTT_LIST } from '../data/boycottList.js'
+
+import {
+  analysePortfolio,
+  calculateTotalInvestment,
+  calculateTotalPortfolioValue,
+  calculatePortfolioChange,
+  calculateTopHoldings,
+  calculateAssetAllocation,
+  calculateSectorExposure,
+  calculateDiversification,
+  calculateRiskTolerance,
+  calculatePortfolioRisk,
+  compareRisk,
+  analyzeEnvironmentalImpact,
+  analyzeSocialImpact,
+} from '../services/portfolioService.js'
